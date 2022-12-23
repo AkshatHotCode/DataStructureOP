1137b428864ee1ca09c536e5a59d1abd8bb34992
Akshat Rajvanshi <akshatrajvanshi14@gmail.com>
2022-12-23 13:04:52 +0530

    Testing

--- Object-Oriented-Programming/Ex_Progression.py
+++ Object-Oriented-Programming/Ex_Progression.py
@@ -46,3 +46,53 @@ class ArithmeticProgression(Progression): #inherit from Progression
         """Update current value by adding the fixed increment."""
         self._current += self._increment
 
+#Geometric Progression
+class GeometricProgression(Progression): #inherit from Progression
+    """Iterator producing an geometric progression."""
+
+    def __init__(self, base=2, start=0):
+        """Create a new arithmetic progression."""
+        super().__init__(start)
+        self._base = base
+
+    def _advance(self):
+        """Update current value by adding the fixed increment."""
+        self._current += self._base
+
+#Fibonacci Progression
+class FibonacciProgression(Progression): #inherit from Progression
+    """Iterator producing an fibonacci progression."""
+
+    def __init__(self, first=2, second=1):
+        """Create a new fibonacci progression."""
+        super().__init__(first)
+        self._prev = second - first
+
+    def _advance(self):
+        """Update current value by taking the sum of previous two"""
+        self._prev, self._current = self._current, self._prev + self._current
+
+
+#+++++++++++++++++++++TESTING OUR PROGRESSION++++++++++++++++++++++++
+if __name__ == '__main__':
+    print("Default Progression: ")
+    Progression().print_progression(10)
+
+    print('Arithmetic Progression with increment 5: ')
+    ArithmeticProgression(5).print_progression(10)
+
+    print('Arithmetic Progression with increment 5 and start 2: ')
+    ArithmeticProgression(5, 2).print_progression(10)
+
+    print('Geometric Progression with default base: ')
+    GeometricProgression().print_progression(10)
+
+    print('Geometric Progression with base 3: ')
+    GeometricProgression(3).print_progression(10)
+
+    print('Fibonacci Progression with default start values: ')
+    FibonacciProgression().print_progression(10)
+
+    print('Fibonacci Progression with default start values 4  and  6: ')
+    FibonacciProgression(4, 6).print_progression(10)
+
