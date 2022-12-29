57f70f4d15dacd1e4f0b0241337cdfa763484ae5
Akshat Rajvanshi <akshatrajvanshi14@gmail.com>
2022-12-29 17:20:46 +0530

    Linear Complexity

--- AnalysisOfAlgorithm/Complexties/LinearComlexity.py
+++ AnalysisOfAlgorithm/Complexties/LinearComlexity.py
@@ -11,3 +11,16 @@ def linear_algorithm(items):
 linear_algorithm([14, 11, 26, 45])
 
 """The complexity of the linear_algo() function is linear in the above example since the number of iterations of the for-loop will be equal to the size of the input items array. For instance, if there are 4 items in the items list, the for-loop will be executed 4 times."""
+
+"""An important thing to note is that with large inputs, constants tend to lose value. This is why we typically remove constants from Big-O notation, and an expression such as O(2n) 
+is usually shortened to O(n). Both O(2n) and O(n) are linear - the linear relationship is what matters, not the concrete value. For example, let's modify the linear_algo():"""
+
+def linear_algorithm1(items):
+    for i in items:
+        print(i)
+
+    for i in items:
+        print(i)
+
+"""There are two for-loops that iterate over the input items list. Therefore the complexity of the algorithm becomes O(2n), however in the case of infinite items in the input list,
+the twice of infinity is still equal to infinity. We can ignore the constant 2 (since it is ultimately insignificant) and the complexity of the algorithm remains O(n)."""
