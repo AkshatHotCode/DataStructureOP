aed3efd8686e3b6adb54d1d6afca5bbd5a88cc3a
Akshat Rajvanshi <akshatrajvanshi14@gmail.com>
2022-12-29 17:03:41 +0530

    Constant Complexity

---
+++ AnalysisOfAlgorithm/Complexties/ConstantComplexity.py
@@ -0,0 +1,11 @@
+#1 --> Constant Complexity - O(c)
+
+"""The complexity of the algorithm is said to be constant if the steps required to complete the excution of an element of an algorithm remain constant, irrespective of the number of input."""
+def constant_algorithm(items):
+    result = items[0] * items[0]
+    print(result)
+
+constant_algorithm([14, 11, 26])
+
+"""Therefore, irrespective of the input sixe, or the number of items in the input list items, the algorithm performs only 2 steps: 1st --> Finding the sqaure, 2nf --> Printing.
+Hence the complexity remains constant."""
