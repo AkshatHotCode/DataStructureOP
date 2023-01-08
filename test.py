08d940eb6ed80853ceec28b830b7a351a008997f
Akshat Rajvanshi <akshatrajvanshi14@gmail.com>
2023-01-08 12:36:35 +0530

    Array

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-y = int(input())
-if (y % 400 == 0) or (y % 100 != 0 and y %4 == 0):
-    print(y,"is leap year.")
-else:
-    print(y,"isn't leap year.")
+first_name = "Akshat"
+last_name = "Rajvanshi"
+
+print(first_name + last_name + "good person!")
